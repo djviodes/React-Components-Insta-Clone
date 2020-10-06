2efe8f84653623ee403a4f6615735b4e69f875cf
djviodes <djviodes26@gmail.com>
2020-10-06 15:17:24 -0700

    Cleaned up all code

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -3,18 +3,15 @@ import Comment from './Comment';
 import './Comments.css';
 
 const Comments = props => {
-  // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
   
   return (
     <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
       {comments.map((comment, index) => {
         return <Comment key={index} comment={comment} />
         })}
     </div>
   );
 };
-
 
 export default Comments;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,12 +3,10 @@ import Post from './Post';
 import './Posts.css';
 
 const Posts = (props) => {
-  // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
   return (
     <div className='posts-container-wrapper'>
-      {/* Map through the posts array returning a Post component at each iteration */}
       {posts.map((posts, index) => {
         return (
           <Post 
@@ -18,7 +16,6 @@ const Posts = (props) => {
           />
         )
       })}
-      {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
 };
